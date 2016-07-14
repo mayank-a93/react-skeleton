6498eeef86524e2616c1fff98beb407614f22a0e
Mayank Agarwal <mayank.agarwal@cronj.com>
2016-07-14 20:22:39 +0530

    made basic skeleton

---
+++ src/components/List.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredients = [{"id":1, "text":"paneer"}, {"id":2, "text":"dal"}, {"id":3, "text":"roti"}];
+
+var List = React.createClass({
+	render: function() {
+		var listItems = ingredients.map(function(item) {
+			return <ListItem key={item.id} ingredient={item.text} />;
+		});
+
+		return (<ul>{listItems}</ul>);
+	}	
+});
+
+
+module.exports = List;
